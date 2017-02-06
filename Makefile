6ab8a1e3cf768c748c14397d4b80cf65273062dd
Jason C. McDonald <jcmcdonald@mousepawgames.com>
2017-02-06 15:47:39 -0800

    Added basic Kivy framework, started toolbar.

--- Makefile
+++ Makefile
@@ -1,4 +1,19 @@
+none: help
+
+help:
+	@echo "Elements: A modern music library."
+	@echo
+	@echo "run            Run Elements."
+	@echo "lint           Run pylint3 on the Elements package."
+	@echo "test           Run pytests for Elements."
+
+lint:
+	pylint3 --rcfile=pylintrc elements
+
+run:
+	python3 elements/elements.py
+
 test:
 	python3 -m pytest elements
 
-.PHONY: test
+.PHONY: lint run test
--- elements/elements.py
+++ elements/elements.py
@@ -1,5 +1,30 @@
-def talk():
-    print("Hello, world.")
+import kivy
+kivy.require('1.9.2')
+
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+
+class ElementsWindow(BoxLayout):
+    """
+    Parent class for the app
+    """
+    pass
+
+class ElementsApp(App):
+    """
+    Application-level class, builds the application
+    """
+    def build(self):
+        """
+        Builds the app out of widgets
+        """
+        elements_app = ElementsWindow()
+        return elements_app
+
+if __name__ == '__main__':
+    ElementsApp().run()
 
 def alive():
+    """Check if Elements is detected. We'll drop this once we have some
+    other tests."""
     return True
