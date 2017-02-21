47e6e09834776e1d181616ed4686c4ce3b2e96d6
Jason C. McDonald <jcmcdonald@mousepawgames.com>
2017-02-20 19:38:06 -0800

    Fighting code gremlins.

--- README.md
+++ README.md
@@ -1,13 +1,23 @@
 # Elements
-A modern music library application for music collectors and enthusiasts. Elements will offer custom library curation, advanced queue control, and music suggestions in a clean, snazzy interface.
+
+A modern music library application for music collectors and enthusiasts.
+Elements will offer custom library curation, advanced queue control, and music
+suggestions in a clean, snazzy interface.
 
 ## Authors
-Elements is developed by [Jason C. McDonald](http://www.indelibluepene.com/), a.k.a. CodeMouse92.
+
+Elements is developed by [Jason C. McDonald](http://www.indelibluepen.com/),
+a.k.a. CodeMouse92.
 
 ## Contributing
-We welcome bug reports, pull requests, feedback, and other such contributions on the [Elements GitHub](https://github.com/CodeMouse92/Elements).
 
-Elements is a personal project, meaning that the main developer is focusing primarily on making a music library application that he likes.
+We welcome bug reports, pull requests, feedback, and other such contributions on
+the [Elements GitHub](https://github.com/CodeMouse92/Elements).
+
+Elements is a personal project, meaning that the main developer is focusing
+primarily on making a music library application that he likes.
 
 ## License
-Elements is licensed under the BSD 3-clause license. See LICENSE.md for more information.
+
+Elements is licensed under the BSD 3-clause license. See LICENSE.md for more
+information.
--- elements/elements.py
+++ elements/elements.py
@@ -2,30 +2,78 @@ import kivy
 kivy.require('1.9.2')
 
 from kivy.app import App
-from kivy.uix.floatlayout import FloatLayout
+from kivy.config import Config
+from kivy.clock import Clock
+from kivy.core.window import Window
+from kivy.uix.button import Button
 from kivy.uix.bubble import Bubble
+from kivy.uix.label import Label
+from kivy.uix.floatlayout import FloatLayout
 
 class ElementsWindow(FloatLayout):
     """
     Parent class for the app
     """
-
-    def menu_toggle_tag_bubble(self):
-        print("Yo!" + str(self))
-        bubble = BubbleTags()
-        self.add_widget(bubble)
-        print(self.ids['btnTags'].y)
-        print(bubble.height)
-        bubble.x = self.ids['btnTags'].x + (self.ids['btnTags'].width/2)
-        bubble.y = self.ids['btnTags'].y - bubble.height
-
-class BubbleTags(Bubble):
     pass
+
+class Tooltip(Label):
+    visible = False
+
+class UIButton(Button):
+    """
+    Extending the button class to allow for additional functionality,
+    including tooltips.
+    """
+
+    tooltip = Tooltip()
+
+    def __init__(self, **kwargs):
+        Window.bind(mouse_pos=self.on_mouse_pos)
+        super().__init__(**kwargs)
+
+    def on_mouse_pos(self, *args):
+        if not self.get_root_window():
+            return
+        pos = args[1]
+        #self.tooltip.pos = pos
+        #Clock.unschedule(self.display_tooltip) # cancel scheduled event since I moved the cursor
+
+        if(self.tooltip.visible):
+            self.close_tooltip() # close if it's opened
+
+        if self.collide_point(*self.to_widget(*pos)):
+            #Clock.schedule_once(self.display_tooltip, 1)
+            self.display_tooltip(pos)
+
+    def display_tooltip(self, pos):
+        self.tooltip.visible = True
+        self.tooltip.text=self.info
+        self.tooltip.x = pos[0]
+        #self.tooltip.x = 100
+        self.tooltip.y = pos[1]
+        #self.tooltip.y = 100
+        App.get_running_app().root.add_widget(self.tooltip)
+        print("Open tooltip: " + str(pos) + " vs (" + str(self.tooltip.x) + ", " + str(self.tooltip.y) + ")")
+        print("    " + str(self.tooltip.width) + "x" + str(self.tooltip.height))
+
+    def close_tooltip(self):
+        self.tooltip.visible = False
+        App.get_running_app().root.remove_widget(self.tooltip)
+        print("Close tooltip")
 
 class ElementsApp(App):
     """
     Application-level class, builds the application
     """
+
+    def build_config(self, config):
+        """
+        Configure the application.
+        """
+
+        # Prevent the window from resizing too small. (SDL2 windows only).
+        Config.set('graphics', 'minimum_width', '500')
+        Config.set('graphics', 'minimum_height', '300')
 
     def build(self):
         """
@@ -39,7 +87,6 @@ class ElementsApp(App):
 
         # Create the window.
         elements_app = ElementsWindow()
-
         return elements_app
 
 if __name__ == '__main__':
