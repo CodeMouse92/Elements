4456c92af70d70bec0e7a3435c28c0893bdee4c8
Jason C. McDonald <jcmcdonald@mousepawgames.com>
2017-02-20 20:59:39 -0800

    Added tooltips (thanks Aritodo from #kivy!); colored buttons.

--- elements/elements.py
+++ elements/elements.py
@@ -2,18 +2,21 @@ import kivy
 kivy.require('1.9.2')
 
 from kivy.app import App
-from kivy.config import Config
 from kivy.clock import Clock
+from kivy.compat import PY2
+from kivy.config import Config
 from kivy.core.window import Window
+from kivy.properties import ObjectProperty
 from kivy.uix.button import Button
-from kivy.uix.bubble import Bubble
+#from kivy.uix.bubble import Bubble
 from kivy.uix.label import Label
 from kivy.uix.floatlayout import FloatLayout
 
+
 class ElementsWindow(FloatLayout):
     """
-    Parent class for the app
-    """
+   Parent class for the app
+   """
     pass
 
 class Tooltip(Label):
@@ -25,51 +28,96 @@ class UIButton(Button):
     including tooltips.
     """
 
-    tooltip = Tooltip()
+    # To get the instance of Tooltip above, we have to define a new ObjectProperty().
+    # We're not sure why. (Thanks to Aritodo of #kivy for figuring this out.)
+    tooltip = ObjectProperty()
 
     def __init__(self, **kwargs):
+        """
+        Initialize a new UIButton widget.
+        """
+        # Watch the window's mouse movement event.
         Window.bind(mouse_pos=self.on_mouse_pos)
-        super().__init__(**kwargs)
+        # Derive the initializer from the parent class initializer.
+        if PY2:
+            super(UIButton, self).__init__(**kwargs)
+        else:
+            super().__init__(**kwargs)
+
+        # Define a new Tooltip.
+        self.tooltip = Tooltip()
 
     def on_mouse_pos(self, *args):
+        """
+        Callback for when the mouse moves on the window.
+        """
+        # If we're not in the root window, forget it.
         if not self.get_root_window():
             return
+
+        # Get the position (x,y) from the arguments.
         pos = args[1]
-        #self.tooltip.pos = pos
-        #Clock.unschedule(self.display_tooltip) # cancel scheduled event since I moved the cursor
 
+        # Cancel any scheduled events since the cursor was moved.
+        Clock.unschedule(self.display_tooltip)
+
+        # If we can see the tooltip, remove it.
         if(self.tooltip.visible):
-            self.close_tooltip() # close if it's opened
+            self.close_tooltip()
 
+        # If we are moving the mouse over this widget...
         if self.collide_point(*self.to_widget(*pos)):
-            #Clock.schedule_once(self.display_tooltip, 1)
-            self.display_tooltip(pos)
+            # Schedule the tooltip to appear.
+            Clock.schedule_once(self.display_tooltip, 1)
+            # Set the tooltip's x to the position of the cursor.
+            if(App.get_running_app().root.width - pos[0] < 50):
+                # However, if we're too close to the right edge (and thus would
+                # have cutoff on the tooltip), move its position back a bit.
+                self.tooltip.x = pos[0] - 50
+            else:
+                self.tooltip.x = pos[0]
 
-    def display_tooltip(self, pos):
+            # Set the tooltip's y to the position of the cursor.
+            if(App.get_running_app().root.height - pos[1] < 30):
+                # If we're too close to the top edge (and thus would have
+                # cutoff), move the tooltip down a bit.
+                self.tooltip.y = pos[1] - 30
+                # Also, move it to the right a little more, so the cursor
+                # doesn't cover up part of the first letter.
+                self.tooltip.x = self.tooltip.x + 7
+            else:
+                self.tooltip.y = pos[1]
+
+    def display_tooltip(self, *args):
+        """
+        Display the tooltip. The location is set in the event listener
+        that calls this.
+        """
+        # Mark the tooltip as visible (for event listener reference only)
         self.tooltip.visible = True
+        # Get the tooltip text from this widget's properties in the .kv file.
         self.tooltip.text=self.info
-        self.tooltip.x = pos[0]
-        #self.tooltip.x = 100
-        self.tooltip.y = pos[1]
-        #self.tooltip.y = 100
+        # Add the widget to the app.
         App.get_running_app().root.add_widget(self.tooltip)
-        print("Open tooltip: " + str(pos) + " vs (" + str(self.tooltip.x) + ", " + str(self.tooltip.y) + ")")
-        print("    " + str(self.tooltip.width) + "x" + str(self.tooltip.height))
 
     def close_tooltip(self):
+        """
+        Remove the tooltip from the screen.
+        """
+        # Mark the tooltip as invisible (for event listener reference only).
         self.tooltip.visible = False
+        # Remove the tooltip from the app.
         App.get_running_app().root.remove_widget(self.tooltip)
-        print("Close tooltip")
 
 class ElementsApp(App):
     """
-    Application-level class, builds the application
-    """
+   Application-level class, builds the application
+   """
 
     def build_config(self, config):
         """
-        Configure the application.
-        """
+       Configure the application.
+       """
 
         # Prevent the window from resizing too small. (SDL2 windows only).
         Config.set('graphics', 'minimum_width', '500')
@@ -77,9 +125,9 @@ class ElementsApp(App):
 
     def build(self):
         """
-        This function starts the application by constructing
-        it from widgets and properties.
-        """
+       This function starts the application by constructing
+       it from widgets and properties.
+       """
 
         # Set the title and icon.
         self.title = "Elements"
@@ -94,5 +142,5 @@ if __name__ == '__main__':
 
 def alive():
     """Check if Elements is detected. We'll drop this once we have some
-    other tests."""
+   other tests."""
     return True
